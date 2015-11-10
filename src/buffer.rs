e21d8096657e24f943c88fd0816129cbe7c06edc
Christopher Fu <chrisf1337@gmail.com>
2015-11-09 23:54:43 -0500

    Initial commit

--- src/buffer.rs
+++ src/buffer.rs
@@ -1,3 +1,6 @@
+extern crate unicode_segmentation;
+use self::unicode_segmentation::UnicodeSegmentation;
+
 pub struct Buffer {
     pub contents: Vec<String>,
     pub end: Coord,
@@ -26,17 +29,87 @@ impl Buffer {
     }
 
     pub fn insert_string_at_coord(&mut self, string: &str, coord: Coord) {
-        if string.len() == 0 {
+        if string.is_empty() {
             return;
         }
+
         assert!(coord.y <= self.contents.len());
-        let line = &mut self.contents[coord.y];
-        let char_indices: Vec<(usize, char)> = line.char_indices().collect();
-        for (index, _) in char_indices {
-            if coord.x == index {
-                for (string_index, string_char) in string.char_indices() {
-                    line.insert(index + string_index, string_char);
+        assert!(coord.x < self.contents[coord.y].len());
+
+        let lines: Vec<&str> = string.lines().collect();
+        // Only one line; check if line ends in \n
+        if lines.len() == 1 {
+            let last_char = string.chars().last();
+            if last_char == Some('\n') {
+                let line = self.contents[coord.y].clone();
+                // let char_indices: Vec<(usize, char)>;
+                let char_indices: Vec<(usize, &str)> = UnicodeSegmentation::grapheme_indices(&line[..], true).collect();
+                // {
+                    // let line = &(self.contents[coord.y]);
+                    // char_indices = line.char_indices().collect();
+                // }
+                for (i, &(index, _)) in char_indices.iter().enumerate() {
+                    if coord.x == i {
+                        let mut first_half_string: String;
+                        let second_half_string: String;
+                        {
+                            let (first_half, second_half) = self.contents[coord.y].split_at(index);
+                            first_half_string = first_half.to_string();
+                            first_half_string.push_str(string);
+                            second_half_string = second_half.to_string();
+                        }
+                        self.contents[coord.y] = first_half_string;
+                        self.contents.insert(coord.y + 1, second_half_string);
+                    }
                 }
+            }
+            else {
+                let line_clone = self.contents[coord.y].clone();
+                let line = &mut self.contents[coord.y];
+                // let line_str: &str = &line;
+                // let char_indices: Vec<(usize, char)> = line.char_indices().collect();
+                let char_indices: Vec<(usize, &str)> = UnicodeSegmentation::grapheme_indices(&line_clone[..], true).collect();
+                for (i, &(index, _)) in char_indices.iter().enumerate() {
+                    if coord.x == i {
+                        for (string_index, string_char) in string.char_indices() {
+                            line.insert(index + string_index, string_char);
+                        }
+                    }
+                }
+            }
+        }
+        else {
+            let mut current_line_number = coord.y;
+            let mut second_half_string = "".to_string();
+            let line = self.contents[coord.y].clone();
+            // let char_indices: Vec<(usize, char)> = self.contents[current_line_number].char_indices().collect();
+            let char_indices: Vec<(usize, &str)> = UnicodeSegmentation::grapheme_indices(&line[..], true).collect();
+            for (i, &(index, _)) in char_indices.iter().enumerate() {
+                if coord.x == i {
+                    let mut first_half_string: String;
+                    {
+                        let (first_half, second_half) = self.contents[coord.y].split_at(index);
+                        first_half_string = first_half.to_string();
+                        first_half_string.push_str(&(lines[0].to_string() + "\n"));
+                        second_half_string = second_half.to_string();
+                    }
+                    self.contents[coord.y] = first_half_string;
+                }
+            }
+            current_line_number += 1;
+            for line in lines.iter().take(lines.len() - 1).skip(1) {
+                self.contents.insert(current_line_number, line.to_string() + "\n");
+                current_line_number += 1;
+            }
+            let mut last_line = lines.iter().last().unwrap().to_string();
+            let last_char = string.chars().last();
+            if last_char == Some('\n') {
+                self.contents.insert(current_line_number, last_line.to_string() + "\n");
+                self.contents.insert(current_line_number + 1, second_half_string);
+            }
+            else {
+                last_line.push_str(&second_half_string);
+                self.contents.insert(current_line_number, last_line);
             }
         }
     }
--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,32 @@ fn test_buffer_insert() {
     let mut buffer = Buffer::new();
     buffer.insert_string_at_coord("aí", Coord::new(0, 0));
     assert!(&(buffer.contents[0]) == "aí\n");
-    buffer.insert_string_at_coord("éå", Coord::new(0, 0));
-    assert!(&(buffer.contents[0]) == "éåaí\n");
+    buffer.insert_string_at_coord("éå", Coord::new(1, 0));
+    assert!(&(buffer.contents[0]) == "aéåí\n");
+    buffer.insert_string_at_coord("éå", Coord::new(2, 0));
+    assert!(&(buffer.contents[0]) == "aééååí\n");
+
+    let mut buffer1 = Buffer::new();
+    buffer1.insert_string_at_coord("ab", Coord::new(0, 0));
+    assert!(&(buffer1.contents[0]) == "ab\n");
+    buffer1.insert_string_at_coord("c\n", Coord::new(1, 0));
+    assert!(buffer1.contents.len() == 2);
+    assert!(&(buffer1.contents[0]) == "ac\n");
+    assert!(&(buffer1.contents[1]) == "b\n");
+
+    let mut buffer2 = Buffer::new();
+    buffer2.insert_string_at_coord("áb", Coord::new(0, 0));
+    buffer2.insert_string_at_coord("cd\néf", Coord::new(1, 0));
+    assert!(buffer2.contents.len() == 2);
+    assert!(&(buffer2.contents[0]) == "ácd\n");
+    assert!(&(buffer2.contents[1]) == "éfb\n");
+    buffer2.insert_string_at_coord("gh\nîj\n", Coord::new(2, 1));
+    assert!(buffer2.contents.len() == 4);
+    for line in buffer2.contents.iter() {
+        println!("{}", line);
+    }
+    assert!(&(buffer2.contents[0]) == "ácd\n");
+    assert!(&(buffer2.contents[1]) == "éfgh\n");
+    assert!(&(buffer2.contents[2]) == "îj\n");
+    assert!(&(buffer2.contents[3]) == "b\n");
 }
